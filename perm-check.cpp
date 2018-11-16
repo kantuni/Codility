49fbe53d0ea691b6bcb3e04e7f947e4b2349b7dd
kantuni <henrikh.kantuni@gmail.com>
2018-11-16 13:58:52 -0500

    PermCheck

---
+++ perm-check.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> &a) {
+  sort(a.begin(), a.end());
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] != i + 1) {
+      return 0;
+    }
+  }
+  return 1;
+}
