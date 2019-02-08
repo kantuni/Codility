03f901abc05d2b965fc2507c895405337e3d9488
kantuni <henrikh.kantuni@gmail.com>
2019-02-08 18:16:38 +0400

    FrogRiverOne

---
+++ frog-river-one.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int x, vector<int> &a) {
+  vector<int> f(x + 1, 0);
+  int ans = -1;
+  for (int i = 0; i < (int) a.size(); i++) {
+    if (f[a[i]] == 0) {
+      f[a[i]] = 1;
+      x--;
+    }
+    if (x == 0) {
+      ans = i;
+      break;
+    }
+  }
+  return ans;
+}
