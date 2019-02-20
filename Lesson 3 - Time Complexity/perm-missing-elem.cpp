63e864c39e36833a0be70c0fba61ab44079eb290
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-02-20 23:34:23 +0400

    Update perm-missing-elem.cpp

--- Lesson 3 - Time Complexity/perm-missing-elem.cpp
+++ Lesson 3 - Time Complexity/perm-missing-elem.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int solution(vector<int> &a) {
   int ans = 0;
-  for (int i = 1; i < a.size() + 2; i++) {
+  for (int i = 1; i < (int) a.size() + 2; i++) {
     ans = ans xor i;
   }
-  for (int i = 0; i < a.size(); i++) {
-    ans = ans xor a[i];
+  for (auto ai: a) {
+    ans = ans xor ai;
   }
   return ans;
 }
