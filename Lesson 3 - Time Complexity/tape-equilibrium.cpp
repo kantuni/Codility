a31f9c2fdbb696df4331262440ce6bde50deb691
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-02-20 23:35:07 +0400

    Update tape-equilibrium.cpp

--- Lesson 3 - Time Complexity/tape-equilibrium.cpp
+++ Lesson 3 - Time Complexity/tape-equilibrium.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int solution(vector<int> &a) {
   long long sum = 0;
-  for (int i = 0; i < a.size(); i++) {
-    sum += a[i];
+  for (auto ai: a) {
+    sum += ai;
   }
   long long l = 0, r = 0;
   long long ans = -1;
-  for (int i = 0; i < a.size() - 1; i++) {
+  for (int i = 0; i < (int) a.size() - 1; i++) {
     l += a[i];
     r = sum - l;
     if (ans == -1) {
